f054ad46cca54167f36b2e56bcf8796919005933
KnextKoder <knextkoderklan@gmail.com>
2023-05-26 13:04:57 +0100

    git push v.1.0.0

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
-const PORT = 8000;
-const axios = require('axios');
-const cheerio = require('cheerio');
-const express = require('express');
-
-
+const PORT = 8000
+const axios = require('axios')
+const cheerio = require('cheerio')
+const express = require('express')
 const app = express()
-const url = "https://www.theguardian.com"
+const cors = require('cors')
+app.use(cors())
 
+const url = 'https://www.theguardian.com/uk'
 
-axios(url)
-    .then(response => {
-        const html = response.data
-        const $ = cheerio.load(html)
-        const articles = []
+app.get('/', function (req, res) {
+    res.json('This is my webscraper')
+})
 
-        $('.fc-item__title', html).each(function(){
-            const title = $(this).text()
-            const url = $(this).find('a').attr('href')
-            articles.push({
-                title,
-                url
+app.get('/results', (req, res) => {
+    axios(url)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+            const articles = []
+
+            $('.fc-item__title', html).each(function () { //<-- cannot be a function expression
+                const title = $(this).text()
+                const url = $(this).find('a').attr('href')
+                articles.push({
+                    title,
+                    url
+                })
             })
-        })
-        console.log(articles)
-    }).catch(err => console.log(err))
+            res.json(articles)
+        }).catch(err => console.log(err))
 
-app.listen(PORT, ()=>console.log(`Server Running on PORT ${PORT}`) )
+})
+
+
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
+
